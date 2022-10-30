956fed4ba8034093d74789ac62eb42028418a02d
huyenxam <huyenxam250400@gmail.com>
2022-10-30 17:55:00 +0700

    edit face.py

--- face.py
+++ face.py
@@ -32,13 +32,13 @@ def crawl(url, keyword):
 
     # SEARCH
     try:
-        d_search = browser.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div[3]/div/div/div/div[2]/div/div/div[1]/div")
+        d_search = browser.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div[2]/div/div/div[3]/div/div/div/div[2]/div/div/div[1]/div/div")
         d_search.click()
-        sleep(random.randint(1, 3))
+        sleep(random.randint(1, 2))
         txtSearch = browser.find_element(By.XPATH, "//input[@class='x1i10hfl xggy1nq x1s07b3s x1kdt53j x1yc453h xhb22t3 xb5gni xcj1dhv x2s2ed0 xq33zhf xjyslct xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou xnwf7zb x40j3uw x1s7lred x15gyhx8 x9f619 xzsf02u xdl72j9 x1iyjqo2 xs83m0k xjb2p0i x6prxxf xeuugli x1a2a7pz x1n2onr6 x15h3p50 xm7lytj xsyo7zv xdvlbce xc9qbxq x1g8yoln xo6swyp x1ad04t7 x1glnyev x1ix68h3 x19gujb8']")
         txtSearch.send_keys(keyword)
         txtSearch.send_keys(Keys.ENTER)
-        sleep(random.randint(2, 5))
+        sleep(random.randint(1, 3))
     except:
         print("error search")
 
